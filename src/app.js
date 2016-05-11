7eefd1e76f959ed70a331d0e4ee5aa58b1eb4cfb
Doğan Derya <doganderya59@gmail.com>
2016-05-11 03:39:34 +0300

    https

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 const gluon = require('gluon');
+const pem = require('pem');
 const app = gluon({
-  ready: (app) => {
-    const http = require('http').Server(app);
-    const io = require('socket.io')(http);
+  ready: (app, logger) => {
+    pem.createCertificate({days: 1, selfSigned: true}, function (err, keys) {
+      if (err) {
+        logger.error(err.stack);
+        process.exit(1);
+      }
+      const https = require('https').Server({key: keys.serviceKey, cert: keys.certificate}, app);
+      const io = require('socket.io')(https);
 
-    io.on('connection', (socket) => {
-      socket.on('data', (data) => {
-        socket.broadcast.emit('data', data);
+      io.on('connection', (socket) => {
+        socket.on('data', (data) => {
+          socket.broadcast.emit('data', data);
+        });
       });
-    });
 
-    http.listen('80');
+      https.listen(450);
+    });
   }
 });
